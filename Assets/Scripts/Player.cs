e2f919259da3c98f195135dd0c8229ddcdc9c114
Marc Sevilleja <marc@sevilayha.com>
2020-08-20 18:04:24 -0700

    Updated 2D renderer

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int health = 3;
+    [SerializeField] float moveSpeed = 3f;
+
+    public bool inControl = false;
+
+    Vector2 movement;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
